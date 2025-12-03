4019b0dc44c2751351f168d2690147acbf7ec889
Rollo Velarde <rollovelarde@gmail.com>
2025-12-04 03:20:30 +0800

    edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}"
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
   ],
 theme: {
   extend: {
